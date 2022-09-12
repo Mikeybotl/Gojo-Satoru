3443d52c1359053ea1874957a0010a69e5d0a916
Mikeybotl <113317293+Mikeybotl@users.noreply.github.com>
2022-09-12 09:49:42 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,9 +19,9 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['96891103640']
-global.premium = ['96891103640']
-global.ownernomer = '96891103640'
+global.owner = ['+212625660239']
+global.premium = ['+212625660239']
+global.ownernomer = '+212625660239'
 global.ownername = '♛ 𝐌𝐈𝐊𝐄𝐘 ♛'
 global.botname = '♛ غوجو بوت ♛'
 global.footer = ' غوجو بوت ♛'
